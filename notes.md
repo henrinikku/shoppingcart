e0a09c0ca8f73b6aa8aa3a7f53401761fca01985
Henri Nikku <henrinikku1@gmail.com>
2022-03-16 23:23:32 +0200

    Add a new test case to ensure receipt items are printed in the order that they were scanned

--- notes.md
+++ notes.md
@@ -2,7 +2,7 @@
 
 ## TODO
 - [x] Add a 'Total' line to the receipt. This should be the full price we should charge the customer.
-- [ ] Make the receipt print items in the order that they were scanned.
+- [x] Make the receipt print items in the order that they were scanned.
 - [ ] In some branches of the store, customers want the receipt to show the price first on each line. Without changing the IShoppingCart interface, add a way to support this which allows for other formatting options in the future.
 - [ ] One limitation of the codebase is that every time a change is made, many of the tests need updating. Update or replace the test suite to extend coverage and limit the number of tests which need updating when changes are introduced.
 - [ ] Improve the solution & tests. There are no rules/limitations other than conforming to the IShoppingCart interface - go crazy.
--- python/tests.py
+++ python/tests.py
@@ -19,6 +19,29 @@ class ShoppingCartTest(unittest.TestCase):
             ],
         )
 
+    def test_items_are_printed_in_the_order_they_were_added(self):
+        sc = ShoppingCartConcreteCreator().operation()
+        sc.add_item("banana", 5)
+        sc.add_item("apple", 2)
+        sc.add_item("third", 2)
+        sc.add_item("fourth", 2)
+        sc.add_item("fifth", 2)
+
+        with Capturing() as output:
+            sc.print_receipt()
+
+        self.assertEqual(
+            output,
+            [
+                "banana - 5 - 200",
+                "apple - 2 - 100",
+                "third - 2 - 0",
+                "fourth - 2 - 0",
+                "fifth - 2 - 0",
+                "Total price: 1200",
+            ],
+        )
+
     def test_doesnt_explode_on_mystery_item(self):
         sc = ShoppingCartConcreteCreator().operation()
         sc.add_item("apple", 2)
